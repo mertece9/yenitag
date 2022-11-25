a0d3e704aca257c3e9d50ac87c0947eacb03c7b5
Samilben <108641119+Samilben@users.noreply.github.com>
2022-11-25 20:00:02 +0300

    Update pingallbot.py

--- pingallbot.py
+++ pingallbot.py
@@ -57,7 +57,7 @@ async def everyone(client, message):
           if stopProcess: stopProcess = False
           while len(membersList) > 0 and not stopProcess :
             j = 0
-            text1 = f"text = f"{reasonText}\n\n"
+            text1 = f"text = f"{Text}\n\n"
             try:    
               while j < 6:
                 user = membersList.pop(0)
